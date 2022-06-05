a5faab64cbb9e760c73c236c229366760d8f803e
kingorianderson <kingorianderson@gmail.com>
2022-06-05 11:44:32 +0300

    add two numbers

---
+++ Add two number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1;
+	int num2;
+	int sum;
+	
+cout << "This program is used to add two numbers";
+
+cout << "\n Enter the first numbers: \n";
+cin >> num1;
+
+cout << "\n Enter the second numbers: \n";
+cin >> num2;
+
+sum = num1 + num2;
+
+cout << "\n The total sum is: \n";
+cout << sum;
+
+return 0;
+	
+}
+
+ 
